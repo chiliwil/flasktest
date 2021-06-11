2a681e8747cd61a8ba60d9ec850b6d01605ebb11
wil <wil49709@gmail.com>
2021-06-11 00:28:07 -0400

    [add] test2

--- pythonProject/appdos.py
+++ pythonProject/appdos.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -9,13 +9,20 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item= next(filter(lambda x: x['name'] == name, items), None)
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        # item = {'name': name, 'price': 12.00}
+        # items.append(item)
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "Un item con nombre '{}' ya existe.".f(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
